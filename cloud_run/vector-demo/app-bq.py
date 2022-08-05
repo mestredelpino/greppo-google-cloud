8eb74dd943e8ed7d3b2b98c72c9f3cd0fb98042a
carlosmestredp <carlosmestredp@gmail.com>
2022-08-05 12:01:48 +0200

    Update app-bq.py

--- cloud_run/vector-demo/app-bq.py
+++ cloud_run/vector-demo/app-bq.py
@@ -82,4 +82,4 @@ app.bar_chart(name='Geometry count', description='A bar-cart showing the count o
               x=['polygons', 'lines', 'points'], y=[len(regions_df), len(roads_df), len(cities_df)], color='#984ea3')
 
 
-select1 = app.select(name="First selector", options=["a", "b", "c"], default="a")
+select1 = app.select(name="First selector", options=[cities_df["NAME"]], default="a")
