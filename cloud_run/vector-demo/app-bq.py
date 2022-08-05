3afdafac27514d9bb6f5b23d338e0a3faf53d126
carlosmestredp <carlosmestredp@gmail.com>
2022-08-05 22:30:27 +0200

    Update app-bq.py

--- cloud_run/vector-demo/app-bq.py
+++ cloud_run/vector-demo/app-bq.py
@@ -94,7 +94,7 @@ chosen_region = app.select(name="Choose region", options=region_choice, default=
 
 regions_display = choose_feature(f"{gcp_project}.{dataset}.regions","reg_name,reg_istat_code",'{chosen_region}')
 
-cities_in_region = point_in_polygon("*", "carlos-lab.greppo_vector_demo-cities","carlos-lab.greppo_vector_demo-regions","reg_name",'{chosen_region}')
+# cities_in_region = point_in_polygon("*", "carlos-lab.greppo_vector_demo-cities","carlos-lab.greppo_vector_demo-regions","reg_name",'{chosen_region}')
 
 app.display(name='You chose:', value=regions_display["reg_name"])
 
