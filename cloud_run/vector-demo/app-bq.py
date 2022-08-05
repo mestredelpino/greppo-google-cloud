27f2ecb2470e63037ddfc54aca40d5526165fe33
carlosmestredp <carlosmestredp@gmail.com>
2022-08-05 22:57:03 +0200

    Update app-bq.py

--- cloud_run/vector-demo/app-bq.py
+++ cloud_run/vector-demo/app-bq.py
@@ -84,40 +84,40 @@ city_choice = []
 # Choose region
 
 
-# region_choice = []
+region_choice = []
 
-# for i in regions_df["reg_name"]:
-#     region_choice.append(i)
+for i in regions_df["reg_name"]:
+    region_choice.append(i)
 
-# chosen_region = app.select(name="Choose region", options=region_choice, default=region_choice[0])
+chosen_region = app.select(name="Choose region", options=region_choice, default=region_choice[0])
 
-# regions_display = choose_feature(f"{gcp_project}.{dataset}.regions","reg_name,reg_istat_code",'{chosen_region}')
+regions_display = choose_feature(f"{gcp_project}.{dataset}.regions","reg_name,reg_istat_code",'{chosen_region}')
 
-# cities_in_region = point_in_polygon("*", "carlos-lab.greppo_vector_demo-cities","carlos-lab.greppo_vector_demo-regions","reg_name",'{chosen_region}')
+cities_in_region = point_in_polygon("*", "carlos-lab.greppo_vector_demo-cities","carlos-lab.greppo_vector_demo-regions","reg_name",'{chosen_region}')
 
-# app.display(name='You chose:', value=regions_display["reg_name"])
+app.display(name='You chose:', value=regions_display["reg_name"])
 
 
-# app.vector_layer(
-#     data=regions_display,
-#     name="Regions of Italy",
-#     description="Polygons showing the boundaries of regions of Italy.",
-#     style={"fillColor": "#4daf4a"},
-# )
+app.vector_layer(
+    data=regions_display,
+    name="Regions of Italy",
+    description="Polygons showing the boundaries of regions of Italy.",
+    style={"fillColor": "#4daf4a"},
+)
 
-# text_1 = """
-# ## About the web-app
+text_1 = """
+## About the web-app
 
-# The dashboard shows the boundaries of the regions of Italy as polygons, the 
-# major arterial higways as lines and the major cities of each region as points.
-# """
+The dashboard shows the boundaries of the regions of Italy as polygons, the 
+major arterial higways as lines and the major cities of each region as points.
+"""
 
-# app.display(name='text-1', value=text_1)
+app.display(name='text-1', value=text_1)
 
-# app.display(name='text-2',
-#             value='The following displays the count of polygons, lines and points as a barchart.')
+app.display(name='text-2',
+            value='The following displays the count of polygons, lines and points as a barchart.')
 
-# app.bar_chart(name='Geometry count', description='A bar-cart showing the count of each geometry-type in the datasets.',
-#               x=['polygons', 'lines', 'points'], y=[len(regions_df), len(roads_df), len(cities_df)], color='#984ea3')
+app.bar_chart(name='Geometry count', description='A bar-cart showing the count of each geometry-type in the datasets.',
+              x=['polygons', 'lines', 'points'], y=[len(regions_df), len(roads_df), len(cities_df)], color='#984ea3')
 
 
