2b5e1c10df591e9c94b7c0bbc40e8682a01995f4
carlosmestredp <carlosmestredp@gmail.com>
2022-08-05 23:08:10 +0200

    Update app-bq.py

--- cloud_run/vector-demo/app-bq.py
+++ cloud_run/vector-demo/app-bq.py
@@ -105,19 +105,19 @@ city_choice = []
 #     style={"fillColor": "#4daf4a"},
 # )
 
-# text_1 = """
-# ## About the web-app
+text_1 = """
+## About the web-app
 
-# The dashboard shows the boundaries of the regions of Italy as polygons, the 
-# major arterial higways as lines and the major cities of each region as points.
-# """
+The dashboard shows the boundaries of the regions of Italy as polygons, the 
+major arterial higways as lines and the major cities of each region as points.
+"""
 
-# app.display(name='text-1', value=text_1)
+app.display(name='text-1', value=text_1)
 
-# app.display(name='text-2',
-#             value='The following displays the count of polygons, lines and points as a barchart.')
+app.display(name='text-2',
+            value='The following displays the count of polygons, lines and points as a barchart.')
 
-# app.bar_chart(name='Geometry count', description='A bar-cart showing the count of each geometry-type in the datasets.',
-#               x=['polygons', 'lines', 'points'], y=[len(regions_df), len(roads_df), len(cities_df)], color='#984ea3')
+app.bar_chart(name='Geometry count', description='A bar-cart showing the count of each geometry-type in the datasets.',
+              x=['polygons', 'lines', 'points'], y=[len(regions_df), len(roads_df), len(cities_df)], color='#984ea3')
 
 
